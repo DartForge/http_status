fb0a1b22a80225d17c9fc3bed7a9de71784e1df9
Andrea Cantafio <kk4r.1m@gmail.com>
2017-03-30 22:30:32 +0200

    Restored build script

--- ci/test.sh
+++ ci/test.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-pub get
-
 pub run test -j 1 -r expanded
 
 pub global activate -sgit https://github.com/stablekernel/codecov_dart.git
@@ -11,4 +9,4 @@ pub global run dart_codecov_generator --report-on=lib/ --verbose --no-html
 
 curl -s https://codecov.io/bash > .codecov
 chmod +x .codecov
-./.codecov -f lcov.info -X xcode -t @.codecov-token
+./.codecov -f lcov.info -X xcode
